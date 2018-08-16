1a7bfcb237707698a8f3592c64c8622d4ae27931
joseph-lewis <42229769+joseph-lewis@users.noreply.github.com>
2018-08-16 19:19:11 +1000

    Add a function to error check

--- prac_03/ascii_table.py
+++ prac_03/ascii_table.py
@@ -1,17 +1,19 @@
 LOWER = 33
 UPPER = 127
 
-
-char = input("Enter character: ")
-print("The ASCII code for {} is {}".format(char, ord(char)))
-number = int(input("Enter a number between 33 and 127: "))
-while number < int(LOWER) or number > int(UPPER):
+def main():
+    char = input("Enter character: ")
+    print("The ASCII code for {} is {}".format(char, ord(char)))
     number = int(input("Enter a number between 33 and 127: "))
-print("The character for {} is {}".format(number, (chr(int(number)))))
+    new_number = get_number(number)
+    print("The character for {} is {}".format(new_number, (chr(int(new_number)))))
+    for value in range(LOWER, UPPER + 1):
+        print("{} {}".format(value, chr(value)))
 
-for value in range(LOWER, UPPER + 1):
-    print("{} {}".format(value, chr(value)))
+def get_number(number):
+    while number < int(LOWER) or number > int(UPPER):
+        number = int(input("Enter a number between 33 and 127: "))
+    return number
 
 
-
-
+main()
