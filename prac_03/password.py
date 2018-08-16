b495db91013b993ec95adc8126b209bc42167c63
joseph-lewis <42229769+joseph-lewis@users.noreply.github.com>
2018-08-16 17:39:37 +1000

    Add functions to neaten the program

--- prac_03/password.py
+++ prac_03/password.py
@@ -1,5 +1,18 @@
-password = input("Enter password: ")
-while len(password) < 3:
-    password = input("Enter password longer than 3 characters: ")
-for char in password:
-    print("*", end="")
+def main():
+    password = get_password()
+    loop(password)
+
+
+def loop(password):
+    for char in password:
+        print("*", end="")
+
+
+def get_password():
+    password = input("Enter password: ")
+    while len(password) < 3:
+        password = input("Enter password longer than 3 characters: ")
+    return password
+
+
+main()
