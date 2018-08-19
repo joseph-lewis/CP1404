b624f3cc7c262d7d893cbfbe02f0460fe3820dac
joseph-lewis <42229769+joseph-lewis@users.noreply.github.com>
2018-08-19 20:47:10 +1000

    Add exit message and fix count

--- Assignments/Assessment_1.py
+++ Assignments/Assessment_1.py
@@ -3,7 +3,7 @@ MENU = "Menu:\nL - List all books\nA - Add new book\nM - Mark a book as complete
 
 def main():
     book_info = []
-    file_open = open("books.csv", 'r+')
+    file_open = open("books.csv", 'a+')
     for line in file_open:
         book_info.append(line)
     book_info = refresh_book_list(file_open)
@@ -14,7 +14,13 @@ def main():
     while menu_input.lower() != "q":
         if menu_input.lower() == "l":
             book_info = refresh_book_list(file_open)
-            list_books(book_info)
+            pages_to_read = list_books(book_info)
+            print("{} books.".format(count))
+            if check_required_books(book_info) is True:
+                print("You need to read {} pages in {} books.\n".format(sum(pages_to_read), len(pages_to_read)))
+            else:
+                print("No books left to read. Why not add a new book?")
+
         elif menu_input.lower() == "a":
             book_title, book_author, book_pages, adding_book_list = add_book()
             book_info.append(adding_book_list)
@@ -23,17 +29,21 @@ def main():
         elif menu_input.lower() == "m":
             check = check_required_books(book_info)
             if check is True:
-                list_books(book_info)
+                pages_to_read = list_books(book_info)
+                print("{} books.".format(count))
+                print("You need to read {} pages in {} books.\n".format(sum(pages_to_read), len(pages_to_read)))
                 print("Enter the number of the book to mark as completed")
                 delete_number = int(input(">>> "))
-                mark_book(book_info, delete_number, file_open)
+                mark_book(book_info, delete_number)
                 rewrite_book_file(file_open, book_info)
             else:
                 print("No required books")
+        count = len(book_info)
         print(MENU)
         menu_input = input(">>> ")
-
+    print("{} books saved to books.csv\nHave a nice day :)".format(count))
     file_open.close()
+
 
 def rewrite_book_file(file_open, book_info):
     file_open.truncate(0)
@@ -41,13 +51,14 @@ def rewrite_book_file(file_open, book_info):
     file_open.writelines(book_info)
     file_open.flush()
 
+
 def check_required_books(book_info):
     for line in book_info:
         if ",r" in line:
             return True
 
 
-def mark_book(book_info, delete_number, file_open):
+def mark_book(book_info, delete_number):
     if ",r" in book_info[delete_number - 1]:
         book_info[delete_number - 1] = book_info[delete_number - 1].split(",")[:3]
         book_info[delete_number - 1].append("c\n")
@@ -79,10 +90,10 @@ def refresh_book_list(file_open):
 
 
 def list_books(book_info):
-    i = 0
+    count = 0
     pages_to_read = []
     for line in book_info:
-        i += 1
+        count += 1
         book_title = line.split(",")[0]
         book_author = line.split(",")[1]
         book_pages = line.split(",")[2]
@@ -92,9 +103,8 @@ def list_books(book_info):
         else:
             book_completed = ""
 
-        print("{:<1}{}. {:<40} by {:<20} {:>4} pages".format(book_completed, i, book_title, book_author, book_pages))
-    print("{} books.".format(i))
-    print("You need to read {} pages in {} books.\n".format(sum(pages_to_read), len(pages_to_read)))
+        print("{:<1}{}. {:<40} by {:<20} {:>4} pages".format(book_completed, count, book_title, book_author, book_pages))
+    return pages_to_read
 
 
 main()
