9885578a00ca38b59cb768a4eea838eb1dee6bdb
joseph-lewis <42229769+joseph-lewis@users.noreply.github.com>
2018-08-19 15:34:58 +1000

    Add the add book menu function

--- Assignments/Assessment_1.py
+++ Assignments/Assessment_1.py
@@ -4,26 +4,46 @@ MENU = "Menu:\nL - List all books\nA - Add new book\nM - Mark a book as complete
 def main():
     file_open = open("books.csv", 'a+')
     i = 0
-    file_open.seek(0)
-    book_info = file_open.readlines()
+    book_info = refresh_book_list(file_open)
     count = len(book_info)
     print("Reading Tracker 1.0 - by Joseph Lewis\n{} books loaded from books.csv\n".format(count))
     print(MENU)
     menu_input = input(">>> ")
     while menu_input.lower() != "q":
         if menu_input.lower() == "l":
+            book_info = refresh_book_list(file_open)
+            count = len(book_info)
+            refresh_book_list(file_open)
             pages_to_read = list_books(book_info, i)
             print("{} books.".format(count))
             print("You need to read {} pages in {} books.\n".format(sum(pages_to_read), len(pages_to_read)))
         elif menu_input.lower() == "a":
-            print(input("Title: ")
-            print(input("Author: ")
-            print(input("Pages: "))
-            print(input("")
+            book_author, book_pages, book_title = add_book(file_open)
+            print("{} by {}, ({} pages) added to Reading Tracker\n".format(book_title, book_author, book_pages))
         print(MENU)
         menu_input = input(">>> ")
 
     file_open.close()
+
+
+def add_book(file_open):
+    book_title = input("Title: ")
+    file_open.write(book_title)
+    file_open.write(",")
+    book_author = input("Author: ")
+    file_open.write(book_author)
+    file_open.write(",")
+    book_pages = input("Pages: ")
+    file_open.write(book_pages)
+    file_open.write(",r\n")
+    file_open.flush()
+    return book_author, book_pages, book_title
+
+
+def refresh_book_list(file_open):
+    file_open.seek(0)
+    book_info = file_open.readlines()
+    return book_info
 
 
 def list_books(book_info, i):
