13473d8c92670f0e45861a2af7dc6dc5f28be9a6
Joe <jlewi142@gmail.com>
2018-09-06 03:07:00 +1000

    Add files from prac_05 to get feedback

---
+++ prac_05/state_names.py
@@ -0,0 +1,20 @@
+"""
+CP1404/CP5632 Practical
+State names in a dictionary
+File needs reformatting
+"""
+
+
+STATE_NAMES = {"QLD": "Queensland", "NSW": "New South Wales", "NT": "Northern Territory", "WA": "Western Australia",
+               "ACT": "Australian Capital Territory", "VIC": "Victoria", "TAS": "Tasmania"}
+for state in STATE_NAMES:
+    print("{:<3} is {}".format(state, STATE_NAMES[state]))
+
+state_abbreviation = input("Enter short state: ")
+while state_abbreviation != "":
+    if state_abbreviation.upper() in STATE_NAMES:
+        print(state_abbreviation.upper(), "is", STATE_NAMES[state_abbreviation.upper()])
+    else:
+        print("Invalid short state")
+    state_abbreviation = input("Enter short state: ")
+
